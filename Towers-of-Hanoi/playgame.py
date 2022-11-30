960e12377b44d1c253b5d0c78e11928fbda23bff
Deepesh <deepeshonearth@gmail.com>
2022-11-30 12:12:24 +0530

    moves left functionality added to the game

--- Towers-of-Hanoi/playgame.py
+++ Towers-of-Hanoi/playgame.py
@@ -12,15 +12,23 @@ def main():
 5. Type 'QUIT' and press Return, if its hard for you.\n""")
 
     print("###### MENU ######")
-    print("Enter the level from (1 to 5).")
-    level = int(input())
+    print("Enter the level from (3 to 5).")
+    level = int(input('> '))
 
-    while level not in range(1, 5+1):        
+    # display initial moves
+    displayInitialMoves(level)
+
+    while level not in range(3, 5+1):        
         print('Please enter level in the given range only.')
-        level = int(input())
+        level = int(input('> '))
 
+        # display initial moves
+        displayInitialMoves(level)
+        
     # prepare initial setup based on the level
     towers = initial_setup(level)
+
+    moves = 0
 
     while True: # run a single turn
         # Display towers and discs
@@ -32,13 +40,16 @@ def main():
         # move the disk from rod X to rod Y
         try:
             from_rod.pop_and_put(to_rod)
+            moves += 1
+            displayMoves(towers, level, moves)
+
         except Exception as e:
             print(e)
         
 
         # check if player solved the puzzle
         if len(towers['C']) == level:
-            displayRods(towers, level) # Disply towers for one last time.
+            displayRods(towers, level) # Display towers for one last time.
             print(r"Congratulations! You solved the puzzle .'.\(^_^)'/.!")
             sys.exit()
 
@@ -80,7 +91,7 @@ def askForMove(towers):
 # USER INTERFACE
 def displayRods(rods, total_discs):
     """Display the current state"""
-
+    
     # Display the three towers
     for l in range(total_discs, -1, -1):
         for rod in (rods['A'], rods['B'], rods['C'] ):
@@ -109,6 +120,68 @@ def displayDiscs(width, total_discs):
         disc = '#'*width
         numLabel = str(width).rjust(2, '_')
         print(emptiness + disc + numLabel + disc + emptiness, end='')
+
+def displayInitialMoves(level):
+    if level == 3:
+        print("\nYou have 7 moves to finish this puzzle.\n")
+        print("Moves Left: 7/7")
+
+    elif level == 4:
+        print("\nYou have 15 moves to finish this puzzle.\n")
+        print("Moves Left: 15/15")
+
+    elif level == 5:
+        print("\nYou have 31 moves to finish this puzzle.\n")
+        print("Moves Left: 31/31")
+    
+    else:
+        # we can add more here
+        pass
+
+def displayMoves(towers, level, move_counter):
+    """Displays moves"""
+    # if level is 3
+    if level == 3:
+        # max moves are 7
+        max_moves = 7
+
+    # if level is 4
+    elif level == 4:
+        # max moves are 15
+        max_moves = 15
+
+    # if level is 5
+    elif level == 5:
+        # maxmoves are 31
+        max_moves = 31
+    
+    else:
+        # we can add more here
+        pass
+    
+    # if move counter between 0 and max moves
+    if 0 < move_counter < max_moves:
+        # display moves left
+        print(f"Moves Left: {max_moves - move_counter}/{max_moves}")
+
+    # if moves left is 0 and rod C has
+    # all the discs
+    elif len(towers['C']) == level:
+        # display moves left for one last time
+        print(f"Moves Left: {max_moves - move_counter}/{max_moves}")
+
+    # if moves left is 0 and rod C still
+    # doesn't have all the discs
+    else:
+        # display moves left
+        print(f"Moves Left: {max_moves - move_counter}/{max_moves}")
+
+        # display rods for one last time
+        displayRods(towers, level)
+
+        # show game over message
+        print("Game Over, You ran out of moves!\n")
+        sys.exit()
 
 if __name__ == "__main__":
     main()
